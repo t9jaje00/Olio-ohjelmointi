d030306149035580aad24d77fcf8b631d47737df
Jere Jämsä <jerejamsa@Jere-MBP.lan>
2020-09-26 22:07:31 +0300

    tämä on oikea teht21

--- tehtava21/main.cpp
+++ tehtava21/main.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
+//REVERSE
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    string sana;
+    string syotetty_sana;
+    string reverse_sana;
+    cout << "syötä sana, niin tarkastellaan onko se palindromi" << endl;
+    cin >> sana;
+
+    syotetty_sana = sana;
+
+    reverse(sana.begin(), sana.end());
+
+    reverse_sana = sana;
+
+    if(syotetty_sana == reverse_sana)
+    {
+        cout <<"on toisinpäin:\n"<< reverse_sana <<"\n"<< "tämähän on palindromi!" << endl;
+    }
+
+    else
+
+        cout <<"on toisinpäin:\n"<< reverse_sana <<"\n"<< "eipä ollu palindromi" << endl;
+
+
     return 0;
 }
